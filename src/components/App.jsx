4b0c598603faa2a1751b9ece8d629f804321a038
Felipe <phil.sousa@gmail.com>
2018-11-12 14:16:38 -0800

    babel setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,4 +18,5 @@ var App = () => (
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+
 window.App = App;
