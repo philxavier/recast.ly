8c706c5821e185c41fdde771b01605e0079e349b
Duncan Wallace <duncwa@gmail.com>
2018-11-13 15:22:11 -0800

    Complete creating a dynamic Video Player component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -19,3 +19,4 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
+//finish
